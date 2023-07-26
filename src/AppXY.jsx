23d51e96f3bc0bb252ade39c9452d371177cec08
hanseungwan1 <seungwanhan542@gmail.com>
2023-07-27 00:32:59 +0900

    feat: 마우스 따라가기 과제 완료

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AppXY.css";
 
 export default function AppXY() {
+  const [coordinate, setCoordinate] = useState([0, 0]);
+  const mouseMoveHandler = function (e) {
+    setCoordinate([e.clientX, e.clientY]);
+  };
+
   return (
-    <div className="container">
-      <div className="pointer" />
+    <div className="container" onMouseMove={mouseMoveHandler}>
+      <div
+        className="pointer"
+        style={{ left: `${coordinate[0]}px`, top: `${coordinate[1]}px` }}
+      />
     </div>
   );
 }
